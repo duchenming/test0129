59b49e1723b7821c569a0ad85f0bda0077897053
duchenming <chenming@kth.se>
2022-01-29 13:32:03 +0100

    add code

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    age_list = [1,3,2,1,3,2,1,1,3,3,4,5,3,2,1]
+    age_to_count = {}
+    for a in age_list:
+        if a not in age_to_count:
+            age_to_count[a]=0# age occurs 0 times
+        age_to_count[a]+=1
+    print(age_to_count)
+
+
+'''
+1
+age_to_count = {1:2,3:1,2:1}
+
+'''
+
+
+if __name__=='__main__':
+    main()
